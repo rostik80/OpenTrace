9ac72ef1e55b7f40885c57d6e9c159cb3499ab27
rostik80 <rostikuman@gmail.com>
2026-01-28 00:29:19 +0200

    feat: initialize pool-server module with Vert.x and basic structure

---
+++ pool-server/src/main/java/com/opentrace/pool/PoolApplication.java
@@ -0,0 +1,15 @@
+package com.opentrace.pool;
+
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PoolApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(PoolApplication.class);
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+
+    }
+}
